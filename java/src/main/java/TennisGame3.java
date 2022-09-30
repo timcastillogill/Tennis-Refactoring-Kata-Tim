d996534732985d1dce192c04e0a36496b18ca0c9
timcastillogill <timgillviola@gmail.com>
2022-09-30 10:55:18 +0100

    REFACTOR: Tennis game 3

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -1,58 +1,87 @@
+import tennisGame1.Player;
+
 public class TennisGame3 implements TennisGame {
-    
-    private int player1Points;
-    private int player2Points;
-    private String player1Name;
-    private String player2Name;
 
-    public TennisGame3(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
+	private int player1Points;
+	private int player2Points;
+	private String player1Name;
+	private String player2Name;
+	private Player player;
 
-    public String getScore() {
-        String winningPlayerName;
-        if (isOnGoing()) {
-            return onGoingScore();
+	public TennisGame3(String player1Name, String player2Name) {
+		this.player1Name = player1Name;
+		this.player2Name = player2Name;
+		this.player1 = new Player(player1Name);
+		this.player2 = new Player(player1Name);
+	}
+
+	public String getScore() {
+		if (isOnGoing()) {
+			return onGoingScore();
+		}
+
+		if (player1Points == player2Points) {
+			return "Deuce";
+		}
+
+		setWinningPlayerName();
+
+		if (isAdvantage(setWinningPlayerName())) {
+            return getAdvantageString(setWinningPlayerName());
         }
 
-            if (player1Points == player2Points)
-                return "Deuce";
-            if (player1Points > player2Points) {
-                winningPlayerName = player1Name;
-            }
-            else winningPlayerName = player2Name;
-            int scoreDifference = player1Points - player2Points;
 
-            if (scoreDifference == 1 || scoreDifference == -1) {
-                return "Advantage " + winningPlayerName;
-            }
-            return "Win for " + winningPlayerName;
+        return formatWinString(setWinningPlayerName());
+}
 
+
+
+
+
+
+	private String setWinningPlayerName() {
+		String winningPlayerName;
+		if (player1Points > player2Points) {
+			winningPlayerName = player1Name;
+		} else {
+            winningPlayerName = player2Name;
+        }
+		return winningPlayerName;
+	}
+
+	private static String formatWinString(String winningPlayerName) {
+		return "Win for " + winningPlayerName;
+	}
+
+	private boolean isAdvantage(String winningPlayerName) {
+        int scoreDifference = player1Points - player2Points;
+        return (scoreDifference == 1 || scoreDifference == -1);
+    }
+
+    private static String getAdvantageString(String winningPlayerName) {
+        return "Advantage " + winningPlayerName;
     }
 
     private String onGoingScore() {
-            String[] scoreString = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            String player1ScoreAsString = scoreString[player1Points];
-            String player2ScoreAsString = scoreString[player2Points];
-            if (player1Points == player2Points) {
-                return player1ScoreAsString + "-All";
-            }
-            return player1ScoreAsString + "-" + player2ScoreAsString;
-    }
+		String[] scoreString = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+		String player1ScoreAsString = scoreString[player1Points];
+		String player2ScoreAsString = scoreString[player2Points];
+		if (player1Points == player2Points) {
+			return player1ScoreAsString + "-All";
+		}
+		return player1ScoreAsString + "-" + player2ScoreAsString;
+	}
 
-    private boolean isOnGoing() {
-        return player1Points < 4 && player2Points < 4 && !(player1Points + player2Points == 6);
-    }
+	private boolean isOnGoing() {
+		return player1Points < 4 && player2Points < 4 && !(player1Points + player2Points == 6);
+	}
 
-    public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            player1Points++;
-        else
-            player2Points++;
-    }
-
-
+	public void wonPoint(String playerName) {
+		if (playerName == "player1")
+			player1Points++;
+		else
+			player2Points++;
+	}
 
 
 }
